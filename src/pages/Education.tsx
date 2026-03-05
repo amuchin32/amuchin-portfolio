73bba8fe4a1d3237c5681aab995b04d0007e6ebc
Amuchin Feoley Togba <your@gmail.com>
2026-03-05 08:58:29 -0600

    Fix missing navbar on Education page

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -1,17 +1,23 @@
+import NavigationBar from '@/components/NavigationBar';
 import EducationHeroSection from '@/components/EducationHeroSection';
 import AcademicTimelineSection from '@/components/AcademicTimelineSection';
 import InstitutionShowcaseSection from '@/components/InstitutionShowcaseSection';
 import CertificationsGallerySection from '@/components/CertificationsGallerySection';
 import AcademicDocumentsSection from '@/components/AcademicDocumentsSection';
+import Footer from '@/components/Footer';
 
 export default function Education() {
   return (
-    <div className="min-h-screen">
-      <EducationHeroSection />
-      <AcademicTimelineSection />
-      <InstitutionShowcaseSection />
-      <CertificationsGallerySection />
-      <AcademicDocumentsSection />
+    <div className="min-h-screen flex flex-col bg-background">
+      <NavigationBar />
+      <main className="flex-1">
+        <EducationHeroSection />
+        <AcademicTimelineSection />
+        <InstitutionShowcaseSection />
+        <CertificationsGallerySection />
+        <AcademicDocumentsSection />
+      </main>
+      <Footer />
     </div>
   );
 }
